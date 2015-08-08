a4a4d4e1ab174eacd942d7909258e01c82d65b56
Tim Van Holder <tim.vanholder@gmail.com>
2015-08-08 19:19:38 +0200

    First steps - a solution, and a project for the file types.

---
+++ FileTypes/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle        ("FFXIVUtils.FileTypes")]
+[assembly: AssemblyDescription  ("FFXIVUtils - File Types Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany      ("")]
+[assembly: AssemblyProduct      ("FFXIVUtils")]
+[assembly: AssemblyCopyright    ("Copyright © Tim Van Holder 2015")]
+[assembly: AssemblyTrademark    ("")]
+[assembly: AssemblyCulture      ("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion    ("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

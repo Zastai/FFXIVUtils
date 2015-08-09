298a51815a45fcf6e0592a524f2ac2d9324ac317
Tim Van Holder <tim.vanholder@gmail.com>
2015-08-09 19:14:08 +0200

    Since the header seems to be shared by other character DAT file, it was moved to a separate class.

---
+++ FileTypes/CharacterDataFile.cs
@@ -0,0 +1,21 @@
+﻿// A DAT file stored in a character folder.
+
+// Information initially based on http://ffxivexplorer.fragmenterworks.com/research/macros.txt.
+
+// 16-byte Header:
+//   int16 Unknown
+//   int16 Unknown
+//   int32 FileSize     (not including the header)
+//   int32 PayloadSize  (size of actual data in use (so <= FileSize)
+//   int32 Unknown
+// When the first byte of the payload (i.e. the byte at offset 0x10) is 0xff, this seems to indicate that the rest of the payload is XORed with 0x73.
+// Padding bytes past the payload will be 0x00.
+
+namespace FFXIVUtils.FileTypes {
+
+  /// <summary>A character data file.</summary>
+  public abstract class CharacterDataFile {
+  
+  }
+
+}
--- FileTypes/UserMacros.cs
+++ FileTypes/UserMacros.cs
@@ -1,13 +1,8 @@
-﻿// User Macros (stored in the character folders)
+﻿// The MACRO.DAT file.
 
-// Original information from http://ffxivexplorer.fragmenterworks.com/research/macros.txt.
+// Information initially based on http://ffxivexplorer.fragmenterworks.com/research/macros.txt.
 
-// Fixed Size (286752 / 0x46010)
-// Header:
-//   int16 VersionMajor (1)
-//   int16 VersionMinor (1)
-//   int32 FileSize     (not including the header, so 0x46000)
-//   int32 PayloadSize  (starting from 0x10)
+// Fixed Data Size: 0x46000
 // Payload:
 // - "Encrypted" via XOR with 0x73
 // - 100 sets of 18 blocks (title, icon, key and 15 lines)
@@ -19,8 +14,8 @@
 
 namespace FFXIVUtils.FileTypes {
 
-  /// <summary>A User Macros file.</summary>
-  public sealed class UserMacros {
+  /// <summary>A User Macros file (MACRO.DAT).</summary>
+  public sealed class UserMacros : CharacterDataFile {
   
   }
 

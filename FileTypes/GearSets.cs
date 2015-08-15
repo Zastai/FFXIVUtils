26c5c55c068caa29ee84a6c8559cd79b2a5296b7
Tim Van Holder <tim.vanholder@gmail.com>
2015-08-15 14:23:06 +0200

    Added a class for the gearset data. Added info about the 2 first "unknown" fields for both UserMacros and GearSets.

---
+++ FileTypes/GearSets.cs
@@ -0,0 +1,17 @@
+﻿// The GEARSET.DAT file.
+
+// Unknown 1: always 0x0005 ?
+// Unknown 2: always 0x006A ?
+// Fixed Data Size: 0xAF41
+// Payload:
+// - 0xFF ("encryption" indicator)
+// - ? blocks of gearset info:
+
+namespace FFXIVUtils.FileTypes {
+
+  /// <summary>A Gear Set file (GEARSET.DAT).</summary>
+  public sealed class GearSets : CharacterDataFile {
+  
+  }
+
+}
--- FileTypes/UserMacros.cs
+++ FileTypes/UserMacros.cs
@@ -2,9 +2,11 @@
 
 // Information initially based on http://ffxivexplorer.fragmenterworks.com/research/macros.txt.
 
+// Unknown 1: always 0x0001 ?
+// Unknown 2: always 0x0001 ?
 // Fixed Data Size: 0x46000
 // Payload:
-// - "Encrypted" via XOR with 0x73
+// - 0xFF ("encryption" indicator)
 // - 100 sets of 18 blocks (title, icon, key and 15 lines)
 //   - each block has:
 //     char  Type;         // T(itle) I(con) K(ey) L(ine)
